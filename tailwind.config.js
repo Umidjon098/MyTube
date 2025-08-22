faa7d1e4b947df25dcd0923988c4c4480a5b9f4c
u_toychiyev <umidtuychiev098@gmail.com>
2025-08-22 10:57:19 +0500

    tailwind css bug fix

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}", // agar pages ham ishlatsangiz
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
